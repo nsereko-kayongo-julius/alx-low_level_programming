887890377a052e09e25b34a6d068cc49c2d6e350
nsereko-kayongo-julius <Nserekolauv101@gmail.com>
2023-02-16 18:22:37 -0800

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: write  a message
+ * Return: Always returns 1 to indicate an error occurred
+ */
 int main(void)
 {
     char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
