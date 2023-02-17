b6bfb0e3cc5a6572b6f9f32cdbe50ee82bffd557
nsereko-kayongo-julius <Nserekolauv101@gmail.com>
2023-02-16 16:10:18 -0800

    c progra

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
+ * The main function is the entry point of the program.
+ * It prints the message "Programming is like building a multilingual puzzle,"
+ * using the puts function, and then returns 0 to indicate successful completion.
+ */
+
 int main(void)
 {
-	/* printing */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
