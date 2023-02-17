8f1aef6e7cf0fdd4b26aaf924cfde81750792f89
nsereko-kayongo-julius <Nserekolauv101@gmail.com>
2023-02-16 17:41:48 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* printf */
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message
+ * Return: Always returns 0 to indicate no error 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
