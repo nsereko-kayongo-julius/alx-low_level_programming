224886081cca05659f1b9a21c38c88600a0ddbcd
nsereko-kayongo-julius <Nserekolauv101@gmail.com>
2023-02-16 17:47:28 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  * Description: Prints a message
  * Return: Always returns 0 to indicate no error 
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
