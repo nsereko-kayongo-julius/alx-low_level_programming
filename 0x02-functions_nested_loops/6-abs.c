73126e3296175e0508f0a5637de9381ceef5271e
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-21 14:25:29 +0300

    abolute sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - computes integer value
+* @x: parameter
+*
+*Return: retun 0 for success
+*/
+
+int _abs(int x)
+{
+	int x;
+
+	if (x >= 0 || x < 0)
+		return (x);
+}
