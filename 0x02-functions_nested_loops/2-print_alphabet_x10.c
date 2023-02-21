b6dfd03cc841b7f0410e782dfb79608b788159fb
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-21 18:16:41 +0300

    check lower case characters

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,14 @@
 
 int _islower(int c)
 {
-	int j, i = 0;
+	int c, i = 0;
 
 	while (i <= 10)
 	{
 
-		for (j = 'a'; j <= 'z'; j++)
+		for (c = 'a'; j <= 'z'; j++)
 			_putchar(j);
 
 		_putchar('\n');
 	}
-	return (0);
 }
