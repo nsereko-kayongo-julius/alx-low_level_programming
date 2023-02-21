c2a74702a7cb8fb88d0810f03a581c36544784e3
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-21 17:07:10 +0300

    functions in cwq

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabe - entry point of code
+*
+* Return: retun o for success
+*/
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}
