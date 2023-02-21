6ac7d6742fe81c25caad83d1d1e769cab746b6fc
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-21 17:22:18 +0300

    c functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,8 +15,8 @@ void jack_bauer(void)
 		for (m = 0; m <= 59; m++)
 		{
 		_putchar((h / 10) + '0');
-		_putchar((h % 10) + '0')
-		_putchar(':')
+		_putchar((h % 10) + '0');
+		_putchar(':');
 		_putchar((m /10) + '0');
 		_putchar((m % 10) + '0');
 		_putchar('\n');
