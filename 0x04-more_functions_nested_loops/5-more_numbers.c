f98a4ecd73c5ab632c5d0b600fb5565950b934cf
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-23 17:23:44 +0300

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,16 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			printf("%d",j);
-		}
+			if (j >= 10)
+				_putchar(j / 10 + '0');
 
-	
-	_putchar('\n');
+			_putchar(j % 10 + '0');
+
+		}
+		_putchar('\n');
 	}
 }
