04005b5a70f252a78e8e1c3a35611da666838945
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-24 12:06:41 +0300

    correction

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,15 @@ void print_square(int size)
 	int x;
 	int j;
 
-	for (x = 1; x <= size; x++)
+	for (x = 0; x < size; x++)
 	{
-		for (j = 1; j <= size; j++)
+		for (j = 0; j < size; j++)
 			_putchar('#');
 
 
 		_putchar('\n');
 	}
 
-	if (x <= 0)
+	if (x < 0)
 		_putchar('\n');
 }
