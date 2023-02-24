46a87ee1583fe1cfae4a28345afce29e737a05f0
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-24 13:03:22 +0300

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,15 +17,14 @@ int main(void)
 			printf("FizzBuzz");
 
 		else if (num % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 
 		else if (num % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 
 		else
-			printf("%d", num);
+			printf("%d ", num);
 
-		printf(" ");
 	}
 	printf("\n");
 
