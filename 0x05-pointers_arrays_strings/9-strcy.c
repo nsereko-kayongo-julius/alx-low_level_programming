35267b1683fae78abba964e38bcce917b490543b
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-27 17:57:33 +0300

    c

---
+++ 0x05-pointers_arrays_strings/9-strcy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string
+ *@dest: pointer to dest
+ *@src: string to copy
+ *
+ *Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+	int a;
+
+	while (src[n] != '\0')
+		n++;
+
+	a = 0;
+
+	while (i <= n)
+	{
+		dest[a] = src[a];
+		a++
+	}
+
+	a = a;
+
+	while (i <= n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
