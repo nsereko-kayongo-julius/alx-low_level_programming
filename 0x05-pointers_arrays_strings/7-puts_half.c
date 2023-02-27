6e655c29e9b8d3d7ff447bcf311610a03be136d0
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-27 18:42:34 +0300

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of the string
+* @str: string to divide
+*/
+
+void puts_half(char *str)
+{
+	int i, length = 0;
+
+	while (*(str[a] + length) != '\0')
+		length++;
+
+	int start = length / 2;
+
+	if (length % 2 == 1)
+		start = (length - 1) / 2;
+
+	for (i = start; i < length; i++)
+		_putchar(*(str + i));
+
+	_putchar('\n');
+
+}
