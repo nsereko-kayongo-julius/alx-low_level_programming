2da65a0f5b54daf553f8a8bb2e2f12eead1fc60c
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-27 16:47:23 +0300

    c/

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+
+	int b;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
