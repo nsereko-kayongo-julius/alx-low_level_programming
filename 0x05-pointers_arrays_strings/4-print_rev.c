05422b296b5cf61544d820693632349267551a8b
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-27 18:57:56 +0300

    c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev -prints a string in reverse
+ * @s: a string
+ *
+ * Return: string in reverse
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (s[i] != 0)
+	{
+		x++;
+	}
+
+	while (i > 0)
+	{
+		x--;
+		_putchar(s[i]);
+
+	}
+	_putchar('\n');
+
+}
