a5c233a9881efa6aad417e1b7c8a928471b0f6f3
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-02-27 18:47:57 +0300

    c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,11 @@
 void puts_half(char *str)
 {
 	int i, length = 0;
+	int start = length / 2;
 
 	while (*(str + length) != '\0')
 		length++;
 
-	int start = length / 2;
 
 	if (length % 2 == 1)
 		start = (length - 1) / 2;
