42ad99a34af01c98a2d4f02a2c9253cc164a044a
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-03-08 17:55:04 +0300

    squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate the square root
+ * @n: number whose sqrt is to be print
+ * Return: sqrt if success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	else if (n == 0 || n == 1)
+		return (n);
+
+	else
+	{
+		sqrt = _sqrt_recursion(n / 2) * 2;
+		if (n / sqrt == sqrt)
+			return (sqrt);
+
+		else
+			return (_sqrt_recursion(n / sqrt));
+	}
+
+}
+
