6ab99dd0a23c4aa9d3e22ea07aa49ecbb67034a5
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-03-08 18:41:07 +0300

    is a prime factor

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime_diviser - checks prie numbers
+ * @n: integer to check
+ * @i: integer
+ * Return: 1
+ */
+
+int is_prime_diviser(int n, int i)
+{
+	if (n < 2)
+		return (0);
+	if (i == 1)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (is_prime_diviser(n, i - 1));
+
+}
+
+/**
+ * is_prime_number - checks prie numbers
+ * @n: integer to check
+ * Return: 1
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime_diviser(n, n - 1));
+
+}
