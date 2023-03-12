879f70e0c214a40416df3267a7786cde73b916c0
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-03-12 10:57:36 +0300

    my static lib

--- 0x09-static_libraries/file4.c
+++ 0x09-static_libraries/file4.c
@@ -4,7 +4,7 @@
 * *_strchr - function
 * @s: first char
 * @c: second char
-* Return: 1
+* Return: 2
 */
 
 char *_strchr(char *s, char c)
@@ -13,10 +13,10 @@ char *_strchr(char *s, char c)
 }
 
 /**
- * *_strspn - function
+ * _strspn - function
  * @s: first char
  * @accept: second char
- * Return: 1
+ * Return: 2
  */
 
 unsigned int _strspn(char *s, char *accept)
@@ -24,12 +24,15 @@ unsigned int _strspn(char *s, char *accept)
 
 }
 
- /**
-  * *_strpbrk - function
-  * @s: first char
-  * @accept: second char
-  * Return: 1
-  */
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search
+ * @accept: the set of bytes to look for
+ *
+ * Return: a pointer to the byte in `s`
+ * that matches one of the bytes in `accept`,
+ *         or NULL if no such byte is found.
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
@@ -41,7 +44,7 @@ char *_strpbrk(char *s, char *accept)
  * *_strstr - function
  * @haystack: first char
  * @needle: second char
- * Return: 1
+ * Return: 2
  */
 char *_strstr(char *haystack, char *needle)
 {
