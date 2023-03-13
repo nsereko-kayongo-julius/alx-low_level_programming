12b20e9818bb03e3b299164f9e3a56f2ac13f47e
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-03-13 16:29:53 +0300

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,8 +10,18 @@
 int main(int argc, char *argv[])
 {
 
-	int arg_num = argc - 1;
+	int arg_num;
 
-	printf("%d arguents were recieved from %s\n", arg_num, argv[0]);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (arg_num = 0; *argv; arg_num++, argv++)
+			;
+
+		printf("%d\n", arg_num - 1);
+	}
+
 	return (0);
+
 }
