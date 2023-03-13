d941152dcb813f9ff48a7bdd1a843f21e5944058
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-03-13 15:58:11 +0300

    nuber of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arguent count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
